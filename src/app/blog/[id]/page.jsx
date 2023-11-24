c79d3e3cd9ee9535d6d9bd71389b64ea9a03d1fd
Eric Poulin <eric.poulin.nz@gmail.com>
2023-11-24 13:15:49 +1300

    fix: added author to blog posts

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -53,9 +53,24 @@ export default async function Post({ params }) {
             <p className='text-md text-gray-500 font-medium mt-8'>
               <Date dateString={postData.date} />
             </p>
-            <h1 className="text-3xl lg:text-4xl font-medium tracking-tighter md:tracking-tight text-gray-900 mt-2 mb-6">
+            <h1 className="text-3xl lg:text-4xl font-medium tracking-tighter md:tracking-tight text-gray-900 mt-2 mb-4">
               {postData.title}
             </h1>
+            {postData.author_name && (
+              <div className='flex gap-x-3 items-center mb-12'>
+                <Image 
+                  src={postData.author_image}
+                  alt={postData.author_name}
+                  width={40}
+                  height={40}
+                  className='rounded-full'
+                />
+                <div>
+                  <p className='text-sm font-semibold'>By {postData.author_name}</p>
+                  <p className='text-sm -mt-0.5'>{postData.author_title}</p>
+                </div>
+              </div>
+            )}
             <p className='text-lg text-gray-700'>{postData.intro}</p>
             <div
               className='text-gray-700 mt-8'

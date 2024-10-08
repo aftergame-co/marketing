973d31d5f604d1c227e8aed42100dfe432aee7f3
Eric Poulin <eric.poulin.nz@gmail.com>
2024-10-08 15:42:35 +1300

    values

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -51,7 +51,34 @@ export default function About() {
       role: 'Technical Advisor',
       linkedIn: 'https://www.linkedin.com/in/eli-labes-39a705164',
     },
-  ]
+  ];
+
+  const values = [
+    {
+      name: 'Together at the Table',
+      description: 'Just like in cooperative games, we know success comes from combining strengths. We encourage open communication and collaboration to achieve our shared goals.',
+    },
+    {
+      name: 'Progress, Not Perfection',
+      description: 'The best games are won through steady progress, not waiting for the perfect hand. We focus on moving forward with each action.',
+    },
+    {
+      name: 'Players First, Always',
+      description: 'Like all the best games, we know that delivering an exceptional experience is key. Everything we do starts with understanding and serving our players.',
+    },
+    {
+      name: 'Aim for a New Personal Best',
+      description: 'Settling for good isn’t how you win the game. We consistently improve our strategy, knowing there\'s always room for a stronger play.',
+    },
+    {
+      name: 'Focus on a Strategy',
+      description: 'Like many games we know and love, sometimes it’s best to double down on one strategy and nail it rather than spreading yourself too thin.',
+    },
+    {
+      name: 'Play with Purpose',
+      description: 'Fun is at the heart of everything we do. Whether it\'s building great products or playing great games, we believe that enjoyment is key to success.',
+    },
+  ];
 
   return (
     <main className='pt-20'>
@@ -62,13 +89,13 @@ export default function About() {
       <section
         id="our-story"
         aria-label="The Aftergame story"
-        className="bg-gg-blue py-20 sm:py-32"
+        className="bg-gg-blue pt-20 pb-14 sm:pt-32"
       >
         <Container>
           <p className='mb-10 text-center text-xl text-gray-100'>THE AFTERGAME STORY</p>
           <div className='bg-white rounded-2xl md:mx-20 xl:mx-40 p-6 md:p-20'>{/* lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20"> */}
             {/* <div className='lg:col-span-6 text-lg text-gray-900'> */}
-            <p><span className='text-gg-blue font-bold'>In mid-2022</span>, founders John Brent and Eric Poulin decided to start recording the games they played. Their first instinct was a spreadsheet, but it had to be easy to enter results with dropdowns… and formulas… <i>and pivot tables!</i> At first it was very simple, letting you choose a winner and keeping a tally. This quickly got more complex as they added more and more things to track.</p>
+            <p><span className='text-gg-blue font-bold'>In late-2022</span>, founders John Brent and Eric Poulin decided to start recording the games they played. Their first instinct was a spreadsheet, but it had to be easy to enter results with dropdowns… and formulas… <i>and pivot tables!</i> At first it was very simple, letting you choose a winner and keeping a tally. This quickly got more complex as they added more and more things to track.</p>
 
             <p className='mt-6'>It was soon that the penny dropped… there’s definitely an app for this! A quick search found a handful of apps that seemed to do what they were building. Though as good as some of these apps were, there were some opportunities missed.</p>
 
@@ -83,6 +110,32 @@ export default function About() {
                 </div> */}
           </div>
 
+        </Container>
+      </section>
+      <section
+        id="our-values"
+        aria-label="Our team"
+        className="bg-gg-blue pt-14 pb-20 sm:pb-32"
+      >
+        <Container>
+          <p className='text-center text-xl text-gray-100'>OUR VALUES</p>
+          <ul
+            role="list"
+            className="mx-auto mt-10 grid max-w-2xl grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 text-sm md:gap-y-10 lg:max-w-4xl xl:max-w-none"
+          >
+            {values.map((value) => (
+              <li
+                key={value.name}
+                className="rounded-2xl p-8 bg-white border-2 border-gray-100"
+              >
+                {/* <feature.icon className="h-16 w-16" /> */}
+                <h3 className="mt-6 font-semibold text-gray-900 text-lg">
+                  {value.name}
+                </h3>
+                <p className="mt-2 text-gray-700">{value.description}</p>
+              </li>
+            ))}
+          </ul>
         </Container>
       </section>
       <section

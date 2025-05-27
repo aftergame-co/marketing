ceab6add81e6098b2c6d5c3ba21f488e1c2a2960
Eric Poulin <eric.poulin.nz@gmail.com>
2025-05-28 09:23:50 +1200

    better tests

--- src/app/embedtest/page.jsx
+++ src/app/embedtest/page.jsx
@@ -15,39 +15,51 @@ export default function Contact() {
       <section
         id="contact-us"
         aria-label="Contact us"
-        className="pt-20 sm:pt-32 pb-8 sm:pb-12"
+        className="pt-12 pb-20"
       >
       <Container>
-        <div className="lg:gap-y-20">
-          <div className="relative z-10 mx-auto gap-y-4">
-            <h1 className="text-5xl md:text-5xl xl:text-6xl font-medium tracking-tighter md:tracking-tight text-gray-900">
-              Testing an embedded game list
-            </h1>
-            <div className="w-full max-w-[900px] mt-12 mx-auto">
-              <iframe
-                src="https://aftergame.app/events/19a549af-20fd-4b98-9eea-fbbcca4adb2a/lists/12ae5074-b1f1-4f90-96c5-e03833189929?mode=embed"
-                title="Aftergame game list"
-                width="100%"
-                height="600"
-                className="m-auto mt-12 border self-center"
-                frameborder="0"
-              >
-              </iframe>
-            </div>
-            <h1 className="mt-20 text-5xl md:text-5xl xl:text-6xl font-medium tracking-tighter md:tracking-tight text-gray-900">
-              And now an embedded event schedule
-            </h1>
-            <div className="w-full max-w-[900px] mt-12 mx-auto">
-              <iframe
-                src="https://aftergame.app/events/654c2bd1-7447-423e-bd50-64a50326438b/schedule?mode=embed"
-                title="Aftergame event"
-                width="100%"
-                height="600"
-                className="m-auto mt-12 border self-center"
-                frameborder="0"
-              >
-              </iframe>
-            </div>
+        <div className="mx-auto">
+          <h2 className="text-3xl font-medium text-center text-gray-900">
+            Game list test
+          </h2>
+          <div className="w-full max-w-[900px] mt-4 mx-auto">
+            <iframe
+              src="https://aftergame.app/events/19a549af-20fd-4b98-9eea-fbbcca4adb2a/lists/12ae5074-b1f1-4f90-96c5-e03833189929?mode=embed"
+              title="Aftergame game list"
+              width="100%"
+              height="540"
+              frameborder="0"
+              style={{ border: "1px solid #c6c5d0" }}
+            >
+            </iframe>
+          </div>
+          <h2 className="mt-12 text-3xl font-medium text-center text-gray-900">
+            Group events list test
+          </h2>
+          <div className="w-full max-w-[900px] mt-4 mx-auto">
+            <iframe
+              src="https://aftergame.app/groups/village-meeple-board-game-c-4375/events?mode=embed"
+              title="Aftergame event"
+              width="100%"
+              height="540"
+              frameborder="0"
+              style={{ border: "1px solid #c6c5d0" }}
+            >
+            </iframe>
+          </div>
+          <h2 className="mt-12 text-3xl font-medium text-center text-gray-900">
+            Event schedule test
+          </h2>
+          <div className="w-full max-w-[900px] mt-4 mx-auto">
+            <iframe
+              src="https://aftergame.app/events/654c2bd1-7447-423e-bd50-64a50326438b/schedule?mode=embed"
+              title="Aftergame event"
+              width="100%"
+              height="540"
+              frameborder="0"
+              style={{ border: "1px solid #c6c5d0" }}
+            >
+            </iframe>
           </div>
         </div>
       </Container>

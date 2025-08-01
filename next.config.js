364ffe53db926a0dd065cc34c66eb96ccdda0fb6
Eric Poulin <eric.poulin.nz@gmail.com>
2025-08-01 15:53:33 +1200

    chore: next.js upgrade

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   distDir: 'out',
   experimental: {
-    appDir: true,
     scrollRestoration: true,
   },
 }
